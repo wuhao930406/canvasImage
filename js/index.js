41c828a8cdfcd9b3171e7ecd27359aa28c96d20b
wuhao <wuhao>
2018-12-28 13:59:10 +0800

    arrow color

--- js/index.js
+++ js/index.js
@@ -3,14 +3,22 @@
  */
 var c=document.getElementById("Canvas"),
     width =$(document).width(),
-    height = $(document).height(),
-    dfd = $.Deferred();
+    height = $(document).height()>width/0.615+40?$(document).height():width/0.615+40,
+    dfd = $.Deferred(),
+    getDevicePixelRatio = function (){
+        return window.devicePixelRatio || 1;
+    },
+    pixelTatio = getDevicePixelRatio();
 
+    $(".container").css({height:height});
+    $("#hideText").css({top:0.856*width+120})
 window.onload = function () {
-    c.width = width;
-    c.height = height;
-    var ctx=c.getContext("2d");
+    c.style.width = width + "px";
+    c.style.height = height +"px";
+    c.width = width * pixelTatio;
+    c.height = height * pixelTatio;
 
+    var ctx=c.getContext("2d");
     //预加载
     function preloadImg(list, imgs) {
         var def = $.Deferred(),
@@ -36,7 +44,7 @@ window.onload = function () {
         console.log(def.promise())
         return def.promise();
     }
-    var list = ["./images/bac.jpg","./images/head.png","./images/title01.png"]
+    var list = ["./images/bac.jpg",window.config.headimg,"./images/title01.png","./images/centerimg.png","./images/text.png","./images/qrcode.png"]
     $.when(preloadImg(list, imgs=[])).done(
         function() {
             console.log(imgs)
@@ -44,44 +52,75 @@ window.onload = function () {
                 DrawImage(ctx,imgs[0],{
                     x:0,
                     y:0,
-                    width:width,
-                    height:height
+                    width:width* pixelTatio,
+                    height:height* pixelTatio
                 })
             ).done(
                 DrawImage(ctx,imgs[1],{
-                    x:0.065*width,
-                    y:24,
-                    width:0.14*width,
-                    height:0.14*width
+                    x:(0.065*width)* pixelTatio,
+                    y:24* pixelTatio,
+                    width:(0.14*width)* pixelTatio,
+                    height:(0.14*width)* pixelTatio
                 })
             ).done(
-                FillText(ctx,"天使520",{
-                    font:"2rem Arial",
-                    x:0.205*width+16,
-                    y:0.07*width+30,
+                FillText(ctx,window.config.nickname,{
+                    font:""+1.6* pixelTatio+"rem microsoft yahei",
+                    x:(0.205*width+16)* pixelTatio,
+                    y:(0.07*width+30)* pixelTatio,
                 })
             ).done(
                 FillText(ctx,"年度关键词",{
-                    font:"2.2rem Arial",
-                    x:0.205*width+16,
-                    y:0.07*width+65,
+                    font:""+1.8* pixelTatio+"rem microsoft yahei",
+                    x:(0.205*width+16)* pixelTatio,
+                    y:(0.07*width+65)* pixelTatio,
                 })
             ).done(
                 DrawImage(ctx,imgs[2],{
-                    x:0.24*width,
-                    y:0.07*width+85,
-                    width:0.5*width,
-                    height:0.238*0.5*width,
+                    x:(0.24*width)* pixelTatio,
+                    y:(0.07*width+85)* pixelTatio,
+                    width:(0.5*width)* pixelTatio,
+                    height:(0.238*0.5*width)* pixelTatio,
                 })
+            ).done(
+                DrawImage(ctx,imgs[3],{
+                    x:(0.09*width)* pixelTatio,
+                    y:(0.2*width+90)* pixelTatio,
+                    width:(0.82*width)* pixelTatio,
+                    height:(0.8*0.82*width)* pixelTatio,
+                })
+            ).done(
+                ToDrawMain(ctx)
+            ).done(
+                DrawImage(ctx,imgs[4],{
+                    x:(0.085*width)* pixelTatio,
+                    y:(width+110)* pixelTatio,
+                    width:(0.52*width)* pixelTatio,
+                    height:(0.325*0.52*width)* pixelTatio,
+                })
+            ).done(
+                DrawImage(ctx,imgs[5],{
+                    x:(0.631*width)* pixelTatio,
+                    y:(0.856*width+110)* pixelTatio,
+                    width:(0.296*width)* pixelTatio,
+                    height:(0.296*width*1.07)* pixelTatio,
+                })
+            ).done(
+                $("#copy").attr("src",c.toDataURL("image/png"))
+            ).done(
             )
         })
-
-
-
-
-
-
 }
+
+
+function ToDrawMain(ctx){
+    var txt = window.config.description,
+        html = convertText(txt);
+    $("#hideText").html(html);
+    drawText(ctx);
+}
+
+
+
 
 function DrawImage(ctx,img,config){
     ctx.drawImage(img, config.x, config.y,config.width,config.height);
@@ -92,7 +131,34 @@ function FillText(ctx,text,textconfg){
     ctx.font=textconfg.font;
     ctx.fillText(text,textconfg.x,textconfg.y);
     ctx.save(); // 保存当前ctx的状态
+}
 
 
+function convertText(txt) {
+    var html = txt.replace(/(\S)/ig, '<span>$1</span>');
+    html = html.replace(/\n|\r/ig, '<br>');
+    html = html.replace(/\s/ig, ' ');
+    return html;
+}
+function drawText (ctx) {//绘制文本
+    ctx.font = ''+1.2*pixelTatio+'rem microsoft yahei';
+    ctx.textAlign = 'conter';
+    ctx.textBaseline = "top";
+    ctx.fillStyle = '#006871';
+    $.each($("#hideText span"), function (i, item) {
+        var pos = $(item).position();
+        var txt = $(item).text();
+        ctx.fillText(txt, (pos.left+0.1*width)* pixelTatio, (0.856*width+105+pos.top)*pixelTatio);
+    });
+}
+/*显示/隐藏*/
+function showImage(){
+    $(".imgcontain").fadeIn(200);
+    $(".result").animate({marginTop:24},600)
+}
+
+function hideImage(){
+    $(".result").animate({marginTop:-5000},200)
+    $(".imgcontain").fadeOut(600);
 }
 
